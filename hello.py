adae0df0ec3af1d409f94cf7c05dd48f5afa58c5
typhoonseryi <66803009+typhoonseryi@users.noreply.github.com>
2020-06-28 20:51:55 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ def test(environ, start_response):
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    body = "\n".join(environ.get('QUERY_STRING').split("&"))
     start_response(status, headers)
     return body
