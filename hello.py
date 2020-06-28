b99fa2e01bbd2ed7a1b6fd71ce2df7fee6e2e83c
Sergey Shuktomov <s_shuktomov@mail.ru>
2020-06-28 20:10:35 +0300

    comment

---
+++ hello.py
@@ -0,0 +1,8 @@
+def test(environ, start_response):
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    start_response(status, headers)
+    return body
